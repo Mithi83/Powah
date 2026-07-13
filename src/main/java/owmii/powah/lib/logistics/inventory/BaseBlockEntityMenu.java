4172e5a7b98fb7dc679f1063698f3a7833ed9deb
Mithi83 <28407460+Mithi83@users.noreply.github.com>
2026-07-13 08:13:36 +0200

    BlockEntityTypes

--- src/main/java/owmii/powah/lib/logistics/inventory/BaseBlockEntityMenu.java
+++ src/main/java/owmii/powah/lib/logistics/inventory/BaseBlockEntityMenu.java
@@ -12,7 +12,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.BlockEntityTypes;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.capabilities.Capabilities;
@@ -57,7 +57,7 @@ public abstract class BaseBlockEntityMenu<T extends PowahBaseBlockEntity<?> & II
         if (tile instanceof PowahBaseBlockEntity<?>)
             return (T) tile;
         // What the hell is this?
-        return (T) new PowahBaseBlockEntity<>(BlockEntityType.SIGN, pos, Blocks.AIR.defaultBlockState());
+        return (T) new PowahBaseBlockEntity<>(BlockEntityTypes.SIGN, pos, Blocks.AIR.defaultBlockState());
     }
 
     @Override
