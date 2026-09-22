858311ff864683f7d16e285f5974cdb2626de2a4
Mithi83 <28407460+Mithi83@users.noreply.github.com>
2026-09-22 22:33:10 +0200

    TODO - refactor ReactorOverlayHandler

--- src/main/java/owmii/powah/client/handler/ReactorOverlayHandler.java
+++ src/main/java/owmii/powah/client/handler/ReactorOverlayHandler.java
@@ -22,26 +22,29 @@ import owmii.powah.lib.client.util.RenderTypes;
 public class ReactorOverlayHandler {
     private static final RenderType RENDER_TYPE = RenderTypes.createReactorOverlay(Powah.id("textures/misc/reactor_ov.png"));
 
+    private static final int COLOR_RED = 0xffcf040e;
+    private static final int COLOR_GREEN = 0xff75e096;
+
     public static void onSubmitCustomGeometry(SubmitCustomGeometryEvent event) {
         Minecraft mc = Minecraft.getInstance();
         net.minecraft.world.entity.player.Player player = mc.player;
         if (player == null || mc.level == null)
             return;
-        boolean flag = false;
-        boolean flag1 = false;
+        boolean reactorInHand = false;
+        boolean reactorAmountSufficient = false;
         for (InteractionHand hand : InteractionHand.values()) {
             ItemStack stack = player.getItemInHand(hand);
             if (stack.getItem() instanceof ReactorItem) {
-                flag = true;
+                reactorInHand = true;
                 if (player.getInventory().countItem(stack.getItem()) >= 36) {
-                    flag1 = true;
+                    reactorAmountSufficient = true;
                     break;
                 }
                 break;
             }
         }
 
-        if (!flag)
+        if (!reactorInHand)
             return;
         HitResult result = mc.hitResult;
         if (result instanceof BlockHitResult br) {
@@ -52,19 +55,19 @@ public class ReactorOverlayHandler {
             BlockPos pos = isReplaceable ? br.getBlockPos() : br.getBlockPos().relative(br.getDirection());
             List<BlockPos> list = BlockPos.betweenClosedStream(pos.offset(-1, 0, -1), pos.offset(1, 3, 1)).map(BlockPos::immutable)
                     .toList();
-            int color = 0x75e096;
-            if (!flag1 && !player.isCreative())
-                color = 0xcf040e;
-            if (color != 0xcf040e) {
+            int color = COLOR_GREEN;
+            if (!reactorAmountSufficient && !player.isCreative())
+                color = COLOR_RED;
+            if (color != COLOR_RED) {
                 for (BlockPos blockPos : list) {
                     if (!mc.level.getBlockState(blockPos).canBeReplaced()) {
-                        color = 0xcf040e;
+                        color = COLOR_RED;
                         break;
                     }
                 }
                 List<LivingEntity> entities = mc.level.getEntitiesOfClass(LivingEntity.class, new AABB(pos).inflate(1.0D, 3.0D, 1.0D));
                 if (!entities.isEmpty()) {
-                    color = 0xcf040e;
+                    color = COLOR_RED;
                 }
             }
             var poseStack = event.getPoseStack();
@@ -74,18 +77,16 @@ public class ReactorOverlayHandler {
             poseStack.translate(-projectedView.x, -projectedView.y, -projectedView.z);
 
             poseStack.translate(-1.0D, 0.001D, -1.0D);
-            float r = (color >> 16 & 0xFF) / 255.0F;
-            float g = (color >> 8 & 0xFF) / 255.0F;
-            float b = (color & 0xFF) / 255.0F;
+            var overlayColor = color;
 
             event.getSubmitNodeCollector().submitCustomGeometry(poseStack, RENDER_TYPE, (pose, vertexConsumer) -> {
-                vertexConsumer.addVertex(pose, pos.getX(), pos.getY(), pos.getZ() + 3).setColor(r, g, b, 1.0F).setUv(0.0F, 1.0F)
+                vertexConsumer.addVertex(pose, pos.getX(), pos.getY(), pos.getZ() + 3).setColor(overlayColor).setUv(0.0F, 1.0F)
                         .setLight(Render.MAX_LIGHT);
-                vertexConsumer.addVertex(pose, pos.getX() + 3, pos.getY(), pos.getZ() + 3).setColor(r, g, b, 1.0F).setUv(1.0F, 1.0F)
+                vertexConsumer.addVertex(pose, pos.getX() + 3, pos.getY(), pos.getZ() + 3).setColor(overlayColor).setUv(1.0F, 1.0F)
                         .setLight(Render.MAX_LIGHT);
-                vertexConsumer.addVertex(pose, pos.getX() + 3, pos.getY(), pos.getZ()).setColor(r, g, b, 1.0F).setUv(1.0F, 0.0F)
+                vertexConsumer.addVertex(pose, pos.getX() + 3, pos.getY(), pos.getZ()).setColor(overlayColor).setUv(1.0F, 0.0F)
                         .setLight(Render.MAX_LIGHT);
-                vertexConsumer.addVertex(pose, pos.getX(), pos.getY(), pos.getZ()).setColor(r, g, b, 1.0F).setUv(0.0F, 0.0F)
+                vertexConsumer.addVertex(pose, pos.getX(), pos.getY(), pos.getZ()).setColor(overlayColor).setUv(0.0F, 0.0F)
                         .setLight(Render.MAX_LIGHT);
             });
 
